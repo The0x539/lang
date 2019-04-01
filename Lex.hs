e25f66d7fcdfd1db1664805714b666299d1e9241
The0x539 <the0x539@gmail.com>
2019-04-01 15:27:13 -0500

    that was garbage

--- Lex.hs
+++ Lex.hs
@@ -5,14 +5,6 @@ import Data.Maybe
 x ?: Just y = Just (x:y)
 x ?: Nothing = Nothing
 
-consume :: Eq a => ([a] -> Maybe [a]) -> ([a] -> b) -> [a] -> Maybe (b, [a])
-consume f g input = case f input of
-  Nothing -> Nothing
-  Just x -> case stripPrefix x input of
-    Nothing -> Nothing
-    Just rem -> Just (g x, rem)
-
-
 scan :: (a -> Maybe (b, a)) -> a -> Maybe [b]
 scan f input = case f input of
   Nothing -> Nothing
