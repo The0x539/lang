6fa0277dc1a49af997af735c1e1ad4e6d9c37ef8
The0x539 <the0x539@gmail.com>
2019-04-01 15:23:40 -0500

    simplify one of the helper funcs

--- Lex.hs
+++ Lex.hs
@@ -5,14 +5,12 @@ import Data.Maybe
 x ?: Just y = Just (x:y)
 x ?: Nothing = Nothing
 
-make :: Eq a => ([a] -> Maybe [a]) -> ([a] -> Maybe b) -> [a] -> Maybe (b, [a])
-make f g input = case f input of
+consume :: Eq a => ([a] -> Maybe [a]) -> ([a] -> b) -> [a] -> Maybe (b, [a])
+consume f g input = case f input of
   Nothing -> Nothing
-  Just x -> case g x of
+  Just x -> case stripPrefix x input of
     Nothing -> Nothing
-    Just y -> case (stripPrefix x input) of
-      Nothing -> Nothing
-      Just rem -> Just (y, rem)
+    Just rem -> Just (g x, rem)
 
 
 scan :: (a -> Maybe (b, a)) -> a -> Maybe [b]
